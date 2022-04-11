e9c4e4f8e9fdf1487788216c23fa5143109e6d18
Zac Williams <williamszac20@gmail.com>
2022-04-11 01:15:49 +0100

    server interaction

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHashConnect } from "../../HashConnectAPIProvider"
 
 const Login = () => {
   const { authenticate, connect, walletData, netWork, installedExtensions } = useHashConnect();
   const { accountIds, id } = walletData;
+
+
+  interface State {
+    response: string;
+    post: string;
+    responseToPost: string;
+  }
+
+  const INITIAL_STATE: State = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  const [state, setState] = useState<State>(INITIAL_STATE);
+
+  useEffect(() => {
+    const componentDidMount = () => {
+      callApi()
+        .then(res => setState(prevState => ({ ...prevState, response: res.express })))
+        .catch(err => console.log(err));
+    }
+    componentDidMount();
+  }, []);
+
+  const callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: state.post }),
+    });
+    const body = await response.text();
+    
+    setState(prevState => ({ ...prevState, responseToPost: body }));
+  };
 
   const conCatAccounts = (lastAccs: string, Acc: string) => {
     return lastAccs + " " + Acc;
@@ -49,6 +95,20 @@ const Login = () => {
         <p>
           <button onClick={handleCopy}>Copy Paring String</button>
         </p>
+
+        <p>{state.response}</p>
+        <form onSubmit={handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={state.post}
+            onChange={e => setState( prevState => ({ ...prevState, post: e.target.value }))}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{state.responseToPost}</p>
       </div>
   )
 }
